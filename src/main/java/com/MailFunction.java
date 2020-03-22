e7871ee92b74dfcfef48f2358865e7a9fdfa0801
deepashrangan7 <deepashrangan7>
2020-03-22 19:52:31 +0530

    first commit

---
+++ src/main/java/com/MailFunction.java
@@ -0,0 +1,29 @@
+package com;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class MailFunction {
+
+			@Autowired
+	    private JavaMailSender javaMailSender;
+		
+		public void sendEmail(String email,Integer oid,String status,String name) {
+
+	        SimpleMailMessage msg = new SimpleMailMessage();
+	        msg.setTo(email);
+//	       
+	        msg.setSubject("Order Update ");
+	        msg.setText("Hello "+name+"\n Your Order No : "+oid+"\n is "+status+"\nContact Us Through Help Section If You Have Any Queries \n Call : 8888888888 !.");
+
+	        javaMailSender.send(msg);
+
+	    }
+}
---
+++ src/main/webapp/pages/mainpage.jsp
@@ -0,0 +1,19 @@
+<%@page import="java.util.List"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1" isELIgnored="false"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="f"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Trace</title>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
